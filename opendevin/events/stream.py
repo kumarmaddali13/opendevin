9dd5dcfd816b7a0015e3dafb15b3cb5c83069fc2
Boxuan Li <liboxuan@connect.hku.hk>
2024-05-21 19:29:29 -0700

    Revert "Revert changes to EventStream"
    This reverts commit ba05745dbd334f789a94ef10225cf84ddbd08047.

--- opendevin/events/stream.py
+++ opendevin/events/stream.py
@@ -22,7 +22,9 @@ class EventStreamSubscriber(str, Enum):
 
 class EventStream:
     sid: str
-    _subscribers: dict[str, Callable]
+    # For each subscriber ID, there is a stack of callback functions - useful
+    # when there are agent delegates
+    _subscribers: dict[str, list[Callable]]
     _cur_id: int
     _lock: asyncio.Lock
     _file_store: FileStore
@@ -64,15 +66,17 @@ class EventStream:
 
     def subscribe(self, id: EventStreamSubscriber, callback: Callable):
         if id in self._subscribers:
-            raise ValueError('Subscriber already exists: ' + id)
+            self._subscribers[id].append(callback)
         else:
-            self._subscribers[id] = callback
+            self._subscribers[id] = [callback]
 
     def unsubscribe(self, id: EventStreamSubscriber):
         if id not in self._subscribers:
             logger.warning('Subscriber not found during unsubscribe: ' + id)
         else:
-            del self._subscribers[id]
+            self._subscribers[id].pop()
+            if len(self._subscribers[id]) == 0:
+                del self._subscribers[id]
 
     # TODO: make this not async
     async def add_event(self, event: Event, source: EventSource):
@@ -83,5 +87,6 @@ class EventStream:
         event._source = source  # type: ignore [attr-defined]
         data = event_to_dict(event)
         self._file_store.write(self._get_filename_for_id(event.id), json.dumps(data))
-        for key, fn in self._subscribers.items():
-            await fn(event)
+        for key, stack in self._subscribers.items():
+            callback = stack[-1]
+            await callback(event)
